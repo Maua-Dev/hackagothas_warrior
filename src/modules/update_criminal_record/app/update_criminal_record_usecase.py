514c65031e5e26c1f496433a063c633c08d1a7e6
Gabriel Godoy <gabriel.godoybz@hotmail.com>
2023-04-13 14:22:10 -0300

    update usecase

---
+++ src/modules/update_criminal_record/app/update_criminal_record_usecase.py
@@ -0,0 +1,31 @@
+from src.shared.domain.entities.criminal_record import CriminalRecord
+from src.shared.domain.enums.genre_enum import GENRE_ENUM
+from src.shared.domain.enums.region_enum import REGION_ENUM
+from src.shared.domain.repositories.criminal_record_repository_interface import ICriminalRecordRepository
+from src.shared.helpers.errors.domain_errors import EntityError
+
+
+class UpdateCriminalRecordUsecase:
+    def __init__(self, repository: ICriminalRecordRepository):
+        self.repository = repository
+
+    def __call__(self, criminal_record: CriminalRecord) -> CriminalRecord:
+        if not criminal_record.criminal_record_id != int:
+            raise EntityError("criminal_record_id")
+        if not criminal_record.villain.villain_id != int:
+            raise EntityError("villain_id")
+        if not criminal_record.villain.validate_name(criminal_record.villain.name):
+            raise EntityError("name")
+        if not criminal_record.villain.description != str:
+            raise EntityError("description")
+        if not criminal_record.villain.genre != GENRE_ENUM:
+            raise EntityError("genre")
+        if not criminal_record.villain.region != REGION_ENUM:
+            raise EntityError("region")
+        if not criminal_record.villain.validate_powers(criminal_record.villain.powers):
+            raise EntityError("powers")
+        if not criminal_record.villain.is_arrested != bool:
+            raise EntityError("is_arrested")
+        if not criminal_record.crimes != list:
+            raise EntityError("crimes")
+        return self.repository.update_criminal_record(criminal_record)
